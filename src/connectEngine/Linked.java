7dea634121d749a4d228a53b187b7f2973d5e9ce
Hardita <78241411+OffensiveChip@users.noreply.github.com>
2026-02-19 18:15:51 -0800

    Create Linked.java

---
+++ src/connectEngine/Linked.java
@@ -0,0 +1,15 @@
+package connectEngine;
+
+public class Linked {
+Linked next,prev;
+int val;
+	public Linked(int val) {
+		next = null;
+		this.val = val;
+	}}
+	/*
+	public class List {
+		public void addNode(int nVal) {
+			Linked link = new Linked(nVal);
+		}
+	}*/
